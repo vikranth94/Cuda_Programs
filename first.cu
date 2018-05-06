6dfb10104299100b2f263328612cd6320b7cf76e
Vikranth <vikranth.1994@gmail.com>
2018-05-06 15:55:03 -0700

    1st complete#

--- first.cu
+++ first.cu
@@ -14,5 +14,17 @@ int *d_a, *d_b;
 cudaMalloc(&d_a, sizeof(int));
 cudaMalloc(&d_b, sizeof(int));
 
+cudaMemcpy(d_a, &a, sizeof(int), cudaMemcpyHostToDevice);
+cudaMemcpy(d_b, &b, sizeof(int), cudaMemcpyHostToDevice);
+
+AddIntsCUDA<<<1,1>>>(d_a,d_b);
+
+cudaMemcpy(&a, d_a, sizeof(int), cudaMemcpyDeviceToHost);
+
+cout<<"The answer is"<<a<<endl;
+
+cudaFree(d_a);
+cudaFree(d_b);
+
 return 0;
 }
