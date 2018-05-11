bf657c43ad308fefe2b6cf5056e7286ae3fdcfc5
Vikranth <vikranth.1994@gmail.com>
2018-05-11 13:47:44 -0700

    changes

--- MatrixMultiplicationCode/multShare.c
+++ MatrixMultiplicationCode/multShare.c
@@ -28,7 +28,8 @@ __attribute__ ((noinline))  void end_roi()   {
 
 // Matrix multiplication - Host code 
 // Matrix dimensions are assumed to be multiples of BLOCK_SIZE 
-void MatMul(const Matrix A, const Matrix B, Matrix C) { 
+void MatMul_Shared(const Matrix A, const Matrix B, Matrix C) { 
+
   // Load A and B to device memory 
   Matrix d_A; 
   d_A.width = d_A.stride = A.width; 
@@ -60,7 +61,7 @@ void MatMul(const Matrix A, const Matrix B, Matrix C) {
   dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE); 
   dim3 dimGrid(B.width / dimBlock.x, A.height / dimBlock.y); 
     begin_roi();
-    MatMulKernel<<<dimGrid, dimBlock>>>(d_A, d_B, d_C); 
+    MatMulSharedKernel<<<dimGrid, dimBlock>>>(d_A, d_B, d_C); 
     end_roi();
     err = cudaThreadSynchronize();
     printf("Run kernel: %s\n", cudaGetErrorString(err));
@@ -99,7 +100,7 @@ __device__ Matrix GetSubMatrix(Matrix A, int row, int col) {
 
 
 // Matrix multiplication kernel called by MatMul() 
-__global__ void MatMulKernel(Matrix A, Matrix B, Matrix C) { 
+__global__ void MatMulSharedKernel(Matrix A, Matrix B, Matrix C) { 
   // Block row and column 
   int blockRow = blockIdx.y; 
   int blockCol = blockIdx.x; 
@@ -155,6 +156,70 @@ __global__ void MatMulKernel(Matrix A, Matrix B, Matrix C) {
   SetElement(Csub, row, col, Cvalue); 
 }
 
+void MatMul(const Matrix A, const Matrix B, Matrix C) { 
+
+  // Load A and B to device memory 
+  Matrix d_A; 
+  d_A.width = A.width; 
+  d_A.height = A.height; 
+  size_t size = A.width * A.height * sizeof(float); 
+  cudaError_t err = cudaMalloc(&d_A.elements, size); 
+  printf("CUDA malloc A: %s\n",cudaGetErrorString(err)); 
+  err = cudaMemcpy(d_A.elements, A.elements, size, cudaMemcpyHostToDevice); 
+  printf("Copy A to device: %s\n",cudaGetErrorString(err)); 
+  
+  Matrix d_B; 
+  d_B.width = B.width; 
+  d_B.height = B.height; 
+  size = B.width * B.height * sizeof(float); 
+  err = cudaMalloc(&d_B.elements, size); 
+  printf("CUDA malloc B: %s\n",cudaGetErrorString(err));
+  err = cudaMemcpy(d_B.elements, B.elements, size, cudaMemcpyHostToDevice);
+  printf("Copy B to device: %s\n",cudaGetErrorString(err)); 
+
+  // Allocate C in device memory 
+  Matrix d_C; 
+  d_C.width = C.width; 
+  d_C.height = C.height; 
+  size = C.width * C.height * sizeof(float); 
+  err = cudaMalloc(&d_C.elements, size); 
+  printf("CUDA malloc C: %s\n",cudaGetErrorString(err));
+
+  // Invoke kernel 
+  dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE); 
+  dim3 dimGrid((B.width + dimBlock.x - 1) / dimBlock.x, 
+         (A.height + dimBlock.y - 1) / dimBlock.y);
+  begin_roi(); 
+  MatMulKernel<<<dimGrid, dimBlock>>>(d_A, d_B, d_C); 
+  end_roi();
+  err = cudaThreadSynchronize();
+  printf("Run kernel: %s\n", cudaGetErrorString(err));
+  
+  // Read C from device memory 
+  err = cudaMemcpy(C.elements, d_C.elements, size, cudaMemcpyDeviceToHost); 
+  printf("Copy C off of device: %s\n",cudaGetErrorString(err));
+
+  // Free device memory 
+  cudaFree(d_A.elements); 
+  cudaFree(d_B.elements); 
+  cudaFree(d_C.elements); 
+} 
+
+// Matrix multiplication kernel called by MatMul() 
+__global__ void MatMulKernel(Matrix A, Matrix B, Matrix C) { 
+  // Each thread computes one element of C 
+  // by accumulating results into Cvalue 
+  float Cvalue = 0.0; 
+  int row = blockIdx.y * blockDim.y + threadIdx.y; 
+  int col = blockIdx.x * blockDim.x + threadIdx.x; 
+  if(row > A.height || col > B.width) return;
+  for (int e = 0; e < A.width; ++e) 
+    Cvalue += (A.elements[row * A.width + e]) * (B.elements[e * B.width + col]); 
+  Cvalue = (Cvalue>0) ? Cvalue : Cvalue/4;
+  C.elements[row * C.width + col] = Cvalue; 
+}
+
+
 
 
 int main(int argc, char* argv[]){
@@ -184,6 +249,7 @@ int main(int argc, char* argv[]){
   for(int i = 0; i < B.height; i++)
     for(int j = 0; j < B.width; j++)
       B.elements[i*B.width + j] = (rand() % 2);
+  MatMul_Shared(A, B, C);
   MatMul(A, B, C);
   /*
   for(int i = 0; i < min(10, A.height); i++){
